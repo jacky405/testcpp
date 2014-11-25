c4de4ff11967be8fbbb3f77d6a4df626929d5949
Jacques GUILLOU <jacques.guillou@pelagicore.com>
2014-11-25 16:56:56 +0100

    First commit

---
+++ auto_example.cpp
@@ -0,0 +1,19 @@
+#include <tuple>
+#include <iostream>
+
+using namespace std;
+
+void write() {
+	// nothing to do
+}
+
+template<typename Arg1, typename ... Args>
+void write(const Arg1& firstArg, const Args& ... remainingArguments) {
+	cout << firstArg;
+	write(remainingArguments ...);
+}
+
+int main(int argc, const char** argv) {
+	write("Here is an int : ", 8);
+	cout << endl;
+}
